76c4bae35e6525aff1c0da3906ca9d5990cf05f8
ShrutikaSingh <shrutika051220@gmail.com>
2020-08-15 21:06:17 +0530

    add routes using const route: Routes = [   {     path: 'heroes', component: HeroesComponent   } ];

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-
+const route: Routes = [
+  {
+    path: 'heroes', component: HeroesComponent
+  }
+];
 
 @NgModule({
  exports: [RouterModule]
