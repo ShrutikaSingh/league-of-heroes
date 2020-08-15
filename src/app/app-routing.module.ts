b2a5d0908680577ce183b2fd9a18e4703c98e207
ShrutikaSingh <shrutika051220@gmail.com>
2020-08-15 21:10:12 +0530

    The following line adds the RouterModule to the AppRoutingModule imports array and configures it with the routes in one step by calling RouterModule.forRoot():

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
-const route: Routes = [
+const routes: Routes = [
   {
     path: 'heroes', component: HeroesComponent
   }
 ];
 
 @NgModule({
- exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
